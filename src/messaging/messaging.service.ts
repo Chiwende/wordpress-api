1ac31b4c534c818c6fe625451bd7286177c6de5a
Chiwende Sakala <chiwende@MacBooks-MacBook-Air.local>
2022-07-22 09:33:38 +0200

    updated sms url

--- src/messaging/messaging.service.ts
+++ src/messaging/messaging.service.ts
@@ -4,19 +4,17 @@ import { SMS } from 'src/entities/SendSMS.entity';
 
 @Injectable()
 export class MessagingService {
-    constructor (
-        private readonly httpService: HttpService
-    ) {}
+  constructor(private readonly httpService: HttpService) {}
 
-    async sendTextMessage(payload: SMS){
-        console.log("Send SMS PAyload", payload)
-        const url = 'http://137.184.139.186:8080/sms';
-        return await this.httpService
-          .post(url, payload)
-          .toPromise()
-          .then((res) => {
-            console.log("send SMS response header", res.headers)
-            console.log('Send SMS Response',res.data)         
-          });
-    }
+  async sendTextMessage(payload: SMS) {
+    console.log('Send SMS PAyload', payload);
+    const url = 'http://159.89.88.125:8081/sms';
+    return await this.httpService
+      .post(url, payload)
+      .toPromise()
+      .then((res) => {
+        console.log('send SMS response header', res.headers);
+        console.log('Send SMS Response', res.data);
+      });
+  }
 }
